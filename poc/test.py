e1131e43bd8372620e84e927add5877392c6dd0c
Lahiri, Arindam <arindam.lahiri@infovision.net>
2018-10-31 20:44:20 -0400

    BB

--- poc/test.py
+++ poc/test.py
@@ -1,11 +1,21 @@
 from alpha_vantage.timeseries import TimeSeries
+from alpha_vantage.techindicators import TechIndicators
 import matplotlib.pyplot as plt
 
-ts = TimeSeries(key='LOBRGIUPNJZZLHJX', output_format='pandas',indexing_type='date')
-data, meta_data = ts.get_intraday(symbol='MSFT',interval='5min', outputsize='full')
-print(data.head(5))
-print(data.tail(5))
-print(meta_data)
+my_key='LOBRGIUPNJZZLHJX'
+#ts = TimeSeries(key=my_key, output_format='pandas',indexing_type='date')
+ts = TimeSeries(key=my_key, output_format='pandas')
+data, meta_data = ts.get_intraday(symbol='MSFT',interval='60min', outputsize='full')
+#print(data.head(5))
+#print(data.tail(5))
+#print(meta_data)
 data['4. close'].plot()
 plt.title('Intraday Times Series for the MSFT stock (1 min)')
+plt.show()
+
+
+ti = TechIndicators(key=my_key, output_format='pandas')
+data, meta_data = ti.get_bbands(symbol='MSFT', interval='60min', time_period=60)
+data.plot()
+plt.title('BBbands indicator for  MSFT stock (60 min)')
 plt.show()
