e0ea8ac3a4e5d515a47b99e7fc0dcdd59a20bc8d
Lahiri, Arindam <arindam.lahiri@infovision.net>
2018-10-31 20:21:36 -0400

    head tail on dataframe

--- poc/test.py
+++ poc/test.py
@@ -2,8 +2,10 @@ from alpha_vantage.timeseries import TimeSeries
 import matplotlib.pyplot as plt
 
 ts = TimeSeries(key='LOBRGIUPNJZZLHJX', output_format='pandas',indexing_type='date')
-data, meta_data = ts.get_intraday(symbol='MSFT',interval='1min', outputsize='full')
-print(data.head(2))
+data, meta_data = ts.get_intraday(symbol='MSFT',interval='5min', outputsize='full')
+print(data.head(5))
+print(data.tail(5))
+print(meta_data)
 ##
 # plt.title('Intraday Times Series for the MSFT stock (1 min)')
 ##plt.show()
