d0dd264168ce1c4106c4a75f3f3060ee620ae645
Lahiri, Arindam <arindam.lahiri@infovision.net>
2018-10-31 20:16:27 -0400

    Getting pandas frame

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 ts = TimeSeries(key='LOBRGIUPNJZZLHJX', output_format='pandas',indexing_type='date')
 data, meta_data = ts.get_intraday(symbol='MSFT',interval='1min', outputsize='full')
-#data['close'].plot()
+print(data.head(2))
 ##
 # plt.title('Intraday Times Series for the MSFT stock (1 min)')
 ##plt.show()
